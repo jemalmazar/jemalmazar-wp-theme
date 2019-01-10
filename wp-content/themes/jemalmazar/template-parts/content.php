b957a7c4509db49dfa667de6d179ecaff4fde3ce
Jeremiah Almazar <jemalmazar@gmail.com>
2019-01-09 21:12:32 -0800

    Build the blog feed

--- wp-content/themes/jemalmazar/template-parts/content.php
+++ wp-content/themes/jemalmazar/template-parts/content.php
@@ -10,50 +10,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				jemalmazar_posted_on();
-				jemalmazar_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+	<header class="entry-header">
+		<h2 class="entry-title">
+			<a href='<?php the_permalink() ?>'><?php the_title();?></a>
+		</h2>
+
+		<p class="entry-date"><?php echo get_the_date( 'M d, y' ) ?></p>
 	</header><!-- .entry-header -->
 
-	<?php jemalmazar_post_thumbnail(); ?>
+	<div class="post-description">
+		<?php echo CFS()->get( 'post_description' ); ?>
+	</div>
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'jemalmazar' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+	<hr class="style1">
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jemalmazar' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php jemalmazar_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
